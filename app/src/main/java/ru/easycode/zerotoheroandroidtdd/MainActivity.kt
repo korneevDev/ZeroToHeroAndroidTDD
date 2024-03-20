5dfd8640222ae886ee43e5800908ca23c1de098d
korneevDev <korneev.dev@gmail.com>
2024-03-20 10:51:53 +0300

    Eight task

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     private val viewModel by lazy{
         ViewModelProvider(this)[MainViewModel::class.java]
     }
+
+    private var isDisplay = View.VISIBLE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,5 +24,21 @@ class MainActivity : AppCompatActivity() {
             viewModel.changeState("I am an Android Developer!")
             viewModel.setState(textView)
         }
+
+        findViewById<Button>(R.id.hideButton).setOnClickListener {
+            isDisplay = View.INVISIBLE
+            textView.visibility = isDisplay
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isDisplay = savedInstanceState.getInt("visibility")
+        findViewById<TextView>(R.id.titleTextView).visibility = isDisplay
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("visibility", isDisplay)
     }
 }
