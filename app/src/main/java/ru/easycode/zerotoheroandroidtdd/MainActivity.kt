0ba82e33904145a834a862173dbbc363ab52b216
korneevDev <korneev.dev@gmail.com>
2024-03-20 14:56:32 +0300

    Ninth task

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,24 +2,34 @@ package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel by lazy{
-        ViewModelProvider(this)[MainViewModel::class.java]
-    }
+    private var isRemove = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.titleTextView)
 
-        viewModel.setState(textView)
-        findViewById<Button>(R.id.changeButton).setOnClickListener {
-            viewModel.changeState("I am an Android Developer!")
-            viewModel.setState(textView)
+        findViewById<Button>(R.id.removeButton).setOnClickListener {
+            findViewById<LinearLayout>(R.id.rootLayout).removeView(textView)
+            isRemove = true
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.getBoolean("isRemove")) {
+            findViewById<LinearLayout>(R.id.rootLayout)
+                .removeView(findViewById<TextView>(R.id.titleTextView))
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("isRemove", isRemove)
     }
 }
